ac5335f883e83e782a6338f57a24359dd3d573ed
NgocVT20 <vungoc20092001@g,ail.com>
2024-08-13 23:56:16 +0700

    base

---
+++ src/main/java/com/example/security_base/SecurityBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.security_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/security_base/SecurityBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
